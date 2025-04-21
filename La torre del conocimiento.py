906d9c822481a79ca4b68618b2287c50b6fdcdde
Martín Alejandro García <118487586+MartinAgarcia1710@users.noreply.github.com>
2025-04-21 09:10:40 -0300

    Primer commit del repositorio

---
+++ La torre del conocimiento.py
@@ -0,0 +1,39 @@
+print("Bienvenidos a La torre del conocimiento")
+
+print("para llegar a la cima, debes responder 4 preguntas correctamente")
+respuestas_correctas = 0
+piso_actual = 1
+
+for piso in range(4):
+    
+    if piso_actual == 1:
+        respuesta_correcta = "f"
+        pregunta = "El mes de febrero siempre tiene la misma cantidad de dias"
+
+    elif piso_actual == 2:
+        respuesta_correcta = "f"
+        pregunta = "Estamos en una clase de ingles"
+    elif piso_actual == 3:
+        respuesta_correcta ="f"
+        pregunta = "Messi es un poeta argentino"
+    else:
+        respuesta_correcta = "v"
+        pregunta = "Hoy es viernes"
+            
+    print(pregunta)
+
+    while True:
+        respuesta = input("Ingrese respuesta: ")
+
+        if respuesta == "v" or respuesta == "f":
+            if respuesta == respuesta_correcta:
+                print("Respuesta correcta")
+                piso_actual += 1
+                respuestas_correctas += 1
+                break
+            else:
+                print("Respuesta incorrecta")
+        else:
+            print("Letra ingresada es incorrecta")
+
+print("Felicitaciones, llegaste al piso 5\nJuego Terminado")
---
+++ prueba funciones.py
@@ -0,0 +1,13 @@
+def calcular_edad(anio_nacimiento):
+    print("Calculando edad")
+    return 2025 - anio_nacimiento
+
+
+nac = int(input("Ingrese año de nacimiento"))
+
+edad = calcular_edad(nac)
+
+print(f"Tenes entre {edad - 1} y {edad} años")
+
+
+
